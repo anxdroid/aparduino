4413a9e99edc4ca53acb138fb7f9f5e4c4dc3466
Antonello <paoletti.antonello@gmail.com>
2017-11-16 19:15:14 +0100

    test i2c

---
+++ test_i2c/test_i2c.ino
@@ -0,0 +1,84 @@
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x04
+int number = 0;
+int state = 0;
+int buttonState = 0;
+int buttonPin = 2;
+int relay1Pin = 3;
+int relay1Status = 0;
+int relay2Pin = 4;
+
+void setup() {
+  //pinMode(13, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(relay1Pin, OUTPUT);
+  pinMode(relay2Pin, OUTPUT);
+  Serial.begin(9600); // start serial for output
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+
+  Serial.println("Ready!");
+}
+
+void loop() {
+  delay(100);
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    Serial.println("Button pressed !");
+    toggleRelay();
+  }
+
+}
+
+void toggleRelay() {
+  if (relay1Status == 0) {
+    digitalWrite(relay1Pin, HIGH);
+    Serial.println("ON");
+    relay1Status = 1;
+    delay(100);
+  } else {
+    digitalWrite(relay1Pin, LOW);
+    Serial.println("OFF");
+    relay1Status = 0;
+    delay(100);
+  }
+}
+
+// callback for received data
+void receiveData(int byteCount) {
+
+  while (Wire.available()) {
+    number = Wire.read();
+    Serial.print("data received: ");
+    Serial.println(number);
+
+    if (number == 1) {
+      Serial.println("Command request !");
+      toggleRelay();
+    }
+    /*
+        if (number == 1) {
+
+          if (state == 0) {
+            digitalWrite(13, HIGH); // set the LED on
+            state = 1;
+          }
+          else {
+            digitalWrite(13, LOW); // set the LED off
+            state = 0;
+          }
+        }
+    */
+  }
+}
+
+// callback for sending data
+void sendData() {
+  Wire.write(number);
+}
