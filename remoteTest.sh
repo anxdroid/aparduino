fc315a6c2095c4c316576ca5d08d22c6dc2e9a66
apaolett <Antonello.Paoletti@lnf.infn.it>
2017-11-19 20:20:54 +0100

    update

--- remoteTest.sh
+++ remoteTest.sh
@@ -3,6 +3,6 @@ git add .
 git commit -a -m "update"
 git push
 echo "uploading $1..."
-commands = "cd /home/pi/aparduino; git pull; echo "entering $1"; cd "$1"; make; sudo make upload"
+commands="cd /home/pi/aparduino; git pull; echo "entering $1"; cd "$1"; make; sudo make upload"
 ssh pi@192.168.1.9 '$commands'
 
