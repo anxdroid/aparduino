61f591262bfc5f2f9ad3943a0960279f25db69cd
apaolett <Antonello.Paoletti@lnf.infn.it>
2017-11-19 20:23:25 +0100

    update

--- remoteTest.sh
+++ remoteTest.sh
@@ -5,5 +5,5 @@ git push
 echo "uploading $1..."
 commands="cd /home/pi/aparduino; git pull; cd $1; make;"
 echo "sending commands: $commands"
-ssh pi@192.168.1.9 '$commands'
+ssh pi@192.168.1.9 "$commands"
 
